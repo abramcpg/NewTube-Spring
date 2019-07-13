953416c80fbad55889f3fe2171bea2370f3bf300
Kavya Muppalla <kavyamuppalla@gmail.com>
2019-07-13 19:32:09 -0400

    Comments API

---
+++ videoproject/src/main/java/com/zipcodewilmington/videoproject/models/UserComment.java
@@ -0,0 +1,22 @@
+package com.zipcodewilmington.videoproject.models;
+
+public class UserComment {
+   private String userId;
+   private String text;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
--- videoproject/src/main/java/com/zipcodewilmington/videoproject/repositories/CommentRepository.java
+++ videoproject/src/main/java/com/zipcodewilmington/videoproject/repositories/CommentRepository.java
@@ -2,9 +2,14 @@ package com.zipcodewilmington.videoproject.repositories;
 
 
 import com.zipcodewilmington.videoproject.models.Comment;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, Long> {
+    @Query("SELECT c FROM Comment c WHERE c.videoId = ?1")
+    Collection<Comment> findCommentByVideoId(int videoId);
 }
