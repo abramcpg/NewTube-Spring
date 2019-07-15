7d9d5652046a78a743703d5fbf46cc282a4cdf76
Kavya Muppalla <kavyamuppalla@gmail.com>
2019-07-14 21:07:19 -0400

    UI changes for comments

--- videoproject/src/main/angular-videoproject/src/app/app.module.ts
+++ videoproject/src/main/angular-videoproject/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommentsComponent } from './comments/comments.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- videoproject/src/main/java/com/zipcodewilmington/videoproject/controllers/CommentController.java
+++ videoproject/src/main/java/com/zipcodewilmington/videoproject/controllers/CommentController.java
@@ -13,10 +13,13 @@ public class CommentController {
     @Autowired
     private CommentService commentService;
 
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.GET, value = "/videos/{videoId}/comments")
     public VideoComments getVideoComments(@PathVariable("videoId") int videoId) {
         return commentService.getVideoComments(videoId);
     }
+
+    @CrossOrigin
     @RequestMapping(method = RequestMethod.POST, value = "/videos/{videoId}/comments")
     public void postVideoComment(@RequestBody UserComment userComment, @PathVariable("videoId") int videoId) {
         commentService.postVideoComments(videoId, userComment);
