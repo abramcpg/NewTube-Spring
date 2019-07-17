c73774a2874b5d54c428ad0bd262e7ce76d8cb79
Kavya Muppalla <kavyamuppalla@gmail.com>
2019-07-16 21:08:27 -0400

    User Changes

--- videoproject/src/main/java/com/zipcodewilmington/videoproject/services/UserService.java
+++ videoproject/src/main/java/com/zipcodewilmington/videoproject/services/UserService.java
@@ -2,26 +2,35 @@ package com.zipcodewilmington.videoproject.services;
 
 import com.zipcodewilmington.videoproject.models.Comment;
 import com.zipcodewilmington.videoproject.models.User;
+import com.zipcodewilmington.videoproject.models.UserJson;
 import com.zipcodewilmington.videoproject.repositories.CommentRepository;
 import com.zipcodewilmington.videoproject.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+
 @Service
 public class UserService {
+    @Autowired
     private UserRepository repository;
 
-    @Autowired
-    public UserService(UserRepository repository) {
-        this.repository = repository;
-    }
 
-    public Iterable<User> index(){
-        return repository.findAll();
-    }
+    public UserJson getUserJson(String userId){
 
-    public User create(User user) {
-        return repository.save(user);
+        User user = repository.findById(userId).get();
+        UserJson userJson = new UserJson();
+        userJson.setUserId(user.getUserId());
+        userJson.setFirstName(user.getFirstName());
+        userJson.setLastName(user.getLastName());
+        return userJson;
+    }
+    public void postUserJson(UserJson userJson) {
+        User user = new User();
+        user.setUserId(userJson.getUserId());
+        user.setFirstName(userJson.getFirstName());
+        user.setLastName(userJson.getLastName());
+        repository.save(user);
     }
 
 
