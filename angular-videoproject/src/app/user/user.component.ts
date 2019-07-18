b182f18383887b3cdbb7685a5f17db96fef12458
Kavya Muppalla <kavyamuppalla@gmail.com>
2019-07-18 08:03:55 -0400

    User Create

---
+++ angular-videoproject/src/app/user/user.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../user.service";
+import {User} from "./user";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  private user: User = new User();
+
+  constructor(private service: UserService) { }
+
+  ngOnInit() {
+
+  }
+
+  public addUser() {
+    console.log(this.user);
+    this.service.addUser(this.user);
+  }
+}
