376aa713a3c744e373ab94f20ff9d50f5d1f44d2
abramp20 <abram.p20@gmail.com>
2021-07-12 09:37:14 -0400

    Test commit. No changes.

--- videoproject/src/main/java/com/zipcodewilmington/videoproject/MainApplication.java
+++ videoproject/src/main/java/com/zipcodewilmington/videoproject/MainApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication {
 
+  /*
+  This is not a change. It's a message to test commit features inm GitHub
+   */
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
